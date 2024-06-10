77d0973defe9ab3b3a606ff91fdb63e5510affcb
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-06-10 18:56:50 +0530

    countdowm

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Countdown using callback hell" 
---
+++ js/script.js
@@ -0,0 +1,26 @@
+function displayNumber(number, callback) {
+    document.getElementById('countdown').innerText = number;
+    setTimeout(callback, 1000);
+}
+
+displayNumber(10, function() {
+    displayNumber(9, function() {
+        displayNumber(8, function() {
+            displayNumber(7, function() {
+                displayNumber(6, function() {
+                    displayNumber(5, function() {
+                        displayNumber(4, function() {
+                            displayNumber(3, function() {
+                                displayNumber(2, function() {
+                                    displayNumber(1, function() {
+                                        document.getElementById('countdown').innerText = "Happy Indepedence Day";
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+})
